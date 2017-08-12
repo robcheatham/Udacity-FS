ef600b280ccbe5e99809447a32aca630a19b67ae
Rob Cheatham <robdecipher@Robs-MacBook-Pro.local>
2017-08-12 21:48:21 +0200

    Submission Amends

--- P1/movie_creator.py
+++ P1/movie_creator.py
@@ -2,38 +2,63 @@ import quentin
 import movies
 
 # Movie Instances using the 'movies' Class
-hateful_eight = movies.Movie("The Hateful Eight",
-							 "7.8/10",
-							 "https://goo.gl/yz1RWg",
-							 "https://www.youtube.com/watch?v=6_UI1GzaWv0")
+# Hateful Eight Movie: movie title, movie rating, poster image & trailer
+hateful_eight = movies.Movie(
+    "The Hateful Eight",
+    "7.8/10",
+	"https://goo.gl/yz1RWg",
+    "https://www.youtube.com/watch?v=6_UI1GzaWv0"
+    )
 
-inglourious_basterds = movies.Movie("Inglourious Basterds",
-									"8.3/10",
-									"https://goo.gl/rQv6cZ",
-									"https://www.youtube.com/watch?v=6AtLlVNsuAc")
+# Inglourious Basterds Movie: movie title, movie rating, poster image & trailer
+inglourious_basterds = movies.Movie(
+    "Inglourious Basterds",
+    "8.3/10",
+    "https://goo.gl/rQv6cZ",
+    "https://www.youtube.com/watch?v=6AtLlVNsuAc"
+    )
 
-django_unchained = movies.Movie("Django Unchained",
-								"8.4/10",
-								"https://goo.gl/Zh0ezg",
-								"https://www.youtube.com/watch?v=_iH0UBYDI4g")
+# Django Unchained Movie: movie title, movie rating, poster image & trailer
+django_unchained = movies.Movie(
+    "Django Unchained",
+    "8.4/10",
+    "https://goo.gl/Zh0ezg",
+    "https://www.youtube.com/watch?v=_iH0UBYDI4g"
+    )
 
-jackie_brown = movies.Movie("Jackie Brown",
-							"7.5/10",
-							"https://goo.gl/adK6V0",
-							"https://www.youtube.com/watch?v=HlAECQzTkfY")
+# Jackie Brown Movie: movie title, movie rating, poster image & trailer
+jackie_brown = movies.Movie(
+    "Jackie Brown",
+    "7.5/10",
+    "https://goo.gl/adK6V0",
+    "https://www.youtube.com/watch?v=HlAECQzTkfY"
+    )
 
-pulp_fiction = movies.Movie("Pulp Fiction",
-						   "8.9/10",
-						   "https://goo.gl/VZWHVq",
-						   "https://www.youtube.com/watch?v=tGpTpVyI_OQ")
+# Pulp Fiction Movie: movie title, movie rating, poster image & trailer
+pulp_fiction = movies.Movie(
+    "Pulp Fiction",
+    "8.9/10",
+    "https://goo.gl/VZWHVq",
+    "https://www.youtube.com/watch?v=tGpTpVyI_OQ"
+    )
 
-reservoir_dogs = movies.Movie("Reservoir Dogs",
-							  "8.3/10",
-							  "https://goo.gl/b42X8h",
-							  "https://www.youtube.com/watch?v=vayksn4Y93A")
+# Reservoir Dogs Movie: movie title, movie rating, poster image & trailer
+reservoir_dogs = movies.Movie(
+    "Reservoir Dogs",
+    "8.3/10",
+    "https://goo.gl/b42X8h",
+    "https://www.youtube.com/watch?v=vayksn4Y93A"
+    )
 
 # List for passing to the open movies page function
-movie_list = [hateful_eight, inglourious_basterds,django_unchained, jackie_brown, pulp_fiction, reservoir_dogs]
+movie_list = [
+    hateful_eight,
+    inglourious_basterds,
+    django_unchained,
+    jackie_brown,
+    pulp_fiction,
+    reservoir_dogs
+    ]
 
 # Generate the page with the Movie List
 quentin.open_movies_page(movie_list)
--- P1/movies.py
+++ P1/movies.py
@@ -1,14 +1,16 @@
 import webbrowser
 
+
 class Movie():
 
-	# Constructor Method for a Movie
-	def __init__(self, movie_title, movie_rating, poster_image, movie_trailer_url):
-		self.title = movie_title
-		self.rating = movie_rating
-		self.artwork = poster_image
-		self.trailer = movie_trailer_url
+    # Constructor Method for a Movie
+    def __init__(self, movie_title, movie_rating, poster_image,
+    	         movie_trailer_url):
+        self.title = movie_title
+        self.rating = movie_rating
+        self.artwork = poster_image
+        self.trailer = movie_trailer_url
 
-	# Function to open the Youtube Trailer
-	def open_trailer(self):
-		webbrowser.open(self.trailer)
+    # Function to open the Youtube Trailer
+    def open_trailer(self):
+        webbrowser.open(self.trailer)

0f28dbbad334bc4a47129f03cf2496c719a8d267
Rob Cheatham <robcheatham99@gmail.com>
2017-10-10 18:20:04 +0200

    Separated JS files and updated README

---
+++ P5/js/locations.js
@@ -0,0 +1,48 @@
+// Array of Location Objects for the Map
+var locationList = [
+	{
+		name: 'Boxpark Shoreditch',
+		lat: 51.523704,
+		lng: -0.076474
+	},
+	{
+		name: 'Queen of Hoxton',
+		lat: 51.523704,
+		lng: -0.081235
+	},
+	{
+		name: 'Junkyard Golf Club',
+		lat: 51.521133,
+		lng: -0.072511
+	},
+	{
+		name: 'Bounce, Old Street',
+		lat: 51.526797,
+		lng: -0.084208
+	},
+	{
+		name: 'Flight Club Shoreditch',
+		lat: 51.522172,
+		lng: -0.086588
+	},
+	{
+		name: 'Old Truman Brewery',
+		lat: 51.521887,
+		lng: -0.072257
+	},
+	{
+		name: 'The Breakfast Club',
+		lat: 51.527037,
+		lng: -0.081236
+	},
+	{
+		name: 'Hoxton Grill',
+		lat: 51.525594,
+		lng: -0.082995
+	},
+	{
+		name: 'The Magic Roundabout',
+		lat: 51.525673,
+		lng: -0.087435
+	}
+];

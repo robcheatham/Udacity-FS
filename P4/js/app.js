ac16a4c982193b64c8a7581e9d4e70ad7b6fbe16
Rob Cheatham <robcheatham99@gmail.com>
2017-10-10 11:42:33 +0200

    JS beautified

--- P4/js/app.js
+++ P4/js/app.js
@@ -1,70 +1,65 @@
 var map;
 
 // Array of Location Objects for the Map
-var locationList = [
-	{
-		name: 'Boxpark Shoreditch',
-		lat: 51.523704, 
+var locationList = [{
+        name: 'Boxpark Shoreditch',
+        lat: 51.523704,
         lng: -0.076474
-	},
-	{
-		name: 'Queen of Hoxton',
-		lat: 51.523704, 
+    },
+    {
+        name: 'Queen of Hoxton',
+        lat: 51.523704,
         lng: -0.081235
-	},
-	{
-		name: 'Junkyard Golf Club',
-		lat: 51.521133, 
+    },
+    {
+        name: 'Junkyard Golf Club',
+        lat: 51.521133,
         lng: -0.072511
-	},
-	{
-		name: 'Bounce, Old Street',
-		lat: 51.526797, 
+    },
+    {
+        name: 'Bounce, Old Street',
+        lat: 51.526797,
         lng: -0.084208
-	},
-	{
-		name: 'Flight Club Shoreditch',
-		lat: 51.522172, 
+    },
+    {
+        name: 'Flight Club Shoreditch',
+        lat: 51.522172,
         lng: -0.086588
-	},
-	{
-		name: 'Old Truman Brewery',
-		lat: 51.521887, 
+    },
+    {
+        name: 'Old Truman Brewery',
+        lat: 51.521887,
         lng: -0.072257
-	},
-	{
-		name: 'The Breakfast Club',
-		lat: 51.527037, 
+    },
+    {
+        name: 'The Breakfast Club',
+        lat: 51.527037,
         lng: -0.081236
-	},
-	{
-		name: 'Hoxton Grill',
-		lat: 51.525594, 
+    },
+    {
+        name: 'Hoxton Grill',
+        lat: 51.525594,
         lng: -0.082995
-	},
-	{
-		name: 'The Magic Roundabout',
-		lat: 51.525673, 
+    },
+    {
+        name: 'The Magic Roundabout',
+        lat: 51.525673,
         lng: -0.087435
-	}
+    }
 ]
 
 // Map styling via Snazzy Maps 
-var styles = [
-    {
+var styles = [{
         "featureType": "all",
         "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "color": "#ffffff"
-            }
-        ]
+        "stylers": [{
+            "color": "#ffffff"
+        }]
     },
     {
         "featureType": "all",
         "elementType": "labels.text.stroke",
-        "stylers": [
-            {
+        "stylers": [{
                 "color": "#000000"
             },
             {
@@ -75,17 +70,14 @@ var styles = [
     {
         "featureType": "administrative",
         "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#000000"
-            }
-        ]
+        "stylers": [{
+            "color": "#000000"
+        }]
     },
     {
         "featureType": "administrative",
         "elementType": "geometry.stroke",
-        "stylers": [
-            {
+        "stylers": [{
                 "color": "#144b53"
             },
             {
@@ -99,17 +91,14 @@ var styles = [
     {
         "featureType": "landscape",
         "elementType": "all",
-        "stylers": [
-            {
-                "color": "#08304b"
-            }
-        ]
+        "stylers": [{
+            "color": "#08304b"
+        }]
     },
     {
         "featureType": "poi",
         "elementType": "geometry",
-        "stylers": [
-            {
+        "stylers": [{
                 "color": "#0c4152"
             },
             {
@@ -120,17 +109,14 @@ var styles = [
     {
         "featureType": "road.highway",
         "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#000000"
-            }
-        ]
+        "stylers": [{
+            "color": "#000000"
+        }]
     },
     {
         "featureType": "road.highway",
         "elementType": "geometry.stroke",
-        "stylers": [
-            {
+        "stylers": [{
                 "color": "#0b434f"
             },
             {
@@ -141,17 +127,14 @@ var styles = [
     {
         "featureType": "road.arterial",
         "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#000000"
-            }
-        ]
+        "stylers": [{
+            "color": "#000000"
+        }]
     },
     {
         "featureType": "road.arterial",
         "elementType": "geometry.stroke",
-        "stylers": [
-            {
+        "stylers": [{
                 "color": "#0b3d51"
             },
             {
@@ -162,40 +145,34 @@ var styles = [
     {
         "featureType": "road.local",
         "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#000000"
-            }
-        ]
+        "stylers": [{
+            "color": "#000000"
+        }]
     },
     {
         "featureType": "transit",
         "elementType": "all",
-        "stylers": [
-            {
-                "color": "#146474"
-            }
-        ]
+        "stylers": [{
+            "color": "#146474"
+        }]
     },
     {
         "featureType": "water",
         "elementType": "all",
-        "stylers": [
-            {
-                "color": "#021019"
-            }
-        ]
+        "stylers": [{
+            "color": "#021019"
+        }]
     }
 ]
 
 var ViewModel = function() {
 
-	var self = this;
+    var self = this;
     this.bounds = new google.maps.LatLngBounds();
 
     // Function to provide info when a marker is clicked
     this.populateInfoWindow = function(marker, infowindow) {
-        if(infowindow.marker != marker) {
+        if (infowindow.marker != marker) {
             infowindow.marker = marker;
             // Log lat & lng to the console for checking
             console.log(marker.lat);
@@ -213,12 +190,12 @@ var ViewModel = function() {
             $.getJSON(api, function(data) {
                 var images = data.photos.photo;
                 // Return a url path for each individual image
-                for(var i = 0; i < images.length; i++) {
+                for (var i = 0; i < images.length; i++) {
                     var url = 'https://farm' + images[i].farm + '.staticflickr.com/' + images[i].server + '/' + images[i].id + '_' + images[i].secret + '_q.jpg';
                     // Log all Image url to the console for checking
                     console.log(url);
                     // Create an image path for each image returned from the initial api call
-                    self.content += '<img class="img-thumb" src="'+url+'">';
+                    self.content += '<img class="img-thumb" src="' + url + '">';
                 }
                 self.content += '</div>';
                 infowindow.setContent(self.content);
@@ -245,94 +222,101 @@ var ViewModel = function() {
     };
 
 
-	this.markers = [];
+    this.markers = [];
     // Map Initialisation
-	this.initMap = function() {
+    this.initMap = function() {
 
-		// New Map parameter set up
+        // New Map parameter set up
         map = new google.maps.Map(document.getElementById('map'), {
-			center: {lat: 51.525378, lng: -0.081725},
-			zoom: 15,
-			styles: styles,
-			mapTypeControl: false
-		});
+            center: {
+                lat: 51.525378,
+                lng: -0.081725
+            },
+            zoom: 15,
+            styles: styles,
+            mapTypeControl: false
+        });
 
         this.largeInfoWindow = new google.maps.InfoWindow();
 
-		// Customize Marker Icons
+        // Customize Marker Icons
         var defaultIcon = makeMarkerIcon('ffe600');
-		function makeMarkerIcon(markerColor) {
-	        var markerImage = new google.maps.MarkerImage(
-	          'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
-	          '|40|_|%E2%80%A2',
-	          new google.maps.Size(21, 34),
-	          new google.maps.Point(0, 0),
-	          new google.maps.Point(10, 34),
-	          new google.maps.Size(21,34));
-	        return markerImage;
-	    }
-	    
-		// Loop through Location List to create a Marker for each place
-        for(var i = 0; i < locationList.length; i++) {
-			this.lat = locationList[i].lat;
+
+        function makeMarkerIcon(markerColor) {
+            var markerImage = new google.maps.MarkerImage(
+                'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
+                '|40|_|%E2%80%A2',
+                new google.maps.Size(21, 34),
+                new google.maps.Point(0, 0),
+                new google.maps.Point(10, 34),
+                new google.maps.Size(21, 34));
+            return markerImage;
+        }
+
+        // Loop through Location List to create a Marker for each place
+        for (var i = 0; i < locationList.length; i++) {
+            this.lat = locationList[i].lat;
             this.lng = locationList[i].lng;
-			this.title = locationList[i].name;
-			this.marker = new google.maps.Marker({
+            this.title = locationList[i].name;
+            this.marker = new google.maps.Marker({
                 lat: this.lat,
                 lng: this.lng,
-				position: { lat: this.lat, lng: this.lng },
-				title: this.title,
-				animation: google.maps.Animation.DROP,
-				icon: defaultIcon,
-				id: i
-			});
+                position: {
+                    lat: this.lat,
+                    lng: this.lng
+                },
+                title: this.title,
+                animation: google.maps.Animation.DROP,
+                icon: defaultIcon,
+                id: i
+            });
             // Check for Marker Object
             console.log(this.marker);
             // Display each Marker on the Map
             this.marker.setMap(map);
-			this.markers.push(this.marker);
+            this.markers.push(this.marker);
             // Trigger for opening the InfoWindow
             this.marker.addListener('click', self.clickEvent);
-		}
+        }
 
-		for(var i = 0; i < this.markers.length; i++) {
-			this.markers[i].setMap(map);
-			self.bounds.extend(this.markers[i].position);
-		}
+        for (var i = 0; i < this.markers.length; i++) {
+            this.markers[i].setMap(map);
+            self.bounds.extend(this.markers[i].position);
+        }
 
-		// Ensures map view stays within the bounds of every Marker
-        window.onresize = function () {
+        // Ensures map view stays within the bounds of every Marker
+        window.onresize = function() {
             map.fitBounds(self.bounds);
         };
-	}
-	// Call to initialize the Map
-	this.initMap();
+    }
+    // Call to initialize the Map
+    this.initMap();
 
-	// Filter the List of Locations and Markers
+    // Filter the List of Locations and Markers
     this.filter = ko.observable("");
-	this.locationFilter = ko.computed(function() {
-		var filteredArray = [];
-		for(var i = 0; i < this.markers.length; i++) {
-			var selectedLocation = this.markers[i];
+    this.locationFilter = ko.computed(function() {
+        var filteredArray = [];
+        for (var i = 0; i < this.markers.length; i++) {
+            var selectedLocation = this.markers[i];
             // Compare if the filter value is included in the Location title
-			if(selectedLocation.title.toLowerCase().includes(this.filter().toLowerCase())) {
-				filteredArray.push(selectedLocation);
-				this.markers[i].setVisible(true);
-			} else {
+            if (selectedLocation.title.toLowerCase().includes(this.filter().toLowerCase())) {
+                filteredArray.push(selectedLocation);
+                this.markers[i].setVisible(true);
+            } else {
                 // Hide the marker from the Location list and Map
-				this.markers[i].setVisible(false);
-			}
-		}
-		return filteredArray;
-	}, this);
+                this.markers[i].setVisible(false);
+            }
+        }
+        return filteredArray;
+    }, this);
 }
 
 // Error function in case Map does not Load
 mapLoadError = function() {
-	var mapError = "<span class='map-load-error'>Failed to Load Map. Please try again!</span>";
-	document.getElementById('side-nav').innerHTML = mapError;
+    var mapError = "<span class='map-load-error'>Failed to Load Map. Please try again!</span>";
+    document.getElementById('side-nav').innerHTML = mapError;
 }
 
 function appLoad() {
-	ko.applyBindings(new ViewModel());
+    ko.applyBindings(new ViewModel());
 }

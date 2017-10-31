177702e00db147a293813e3973869998db404d92
Rob Cheatham <robcheatham99@gmail.com>
2017-10-31 17:45:10 +0100

    Google Authentication added

--- P4/backup.py
+++ P4/backup.py
@@ -6,7 +6,8 @@ from sqlalchemy.orm import sessionmaker
 from db_setup import Base, User, Category, Product
 
 from flask import session as login_session
-import random, string
+import random
+import string
 
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.client import FlowExchangeError
@@ -18,7 +19,7 @@ import requests
 app = Flask(__name__)
 
 CLIENT_ID = json.loads(
-    open('client_secrets.json', 'r').read())['web']['client_id']
+	open('client_secrets.json', 'r').read())['web']['client_id']
 APPLICATION_NAME = "Product Catalogue Application"
 
 
@@ -31,70 +32,94 @@ session = DBSession()
 
 
 # Login Page & Create anti-forgery state token
-@app.route('/login/')
+@app.route('/login')
 def showLogin():
-    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
-    login_session['state'] = state
-    return render_template('login.html', STATE=state, navitems=navitems)
+	state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+	login_session['state'] = state
+	return render_template('login.html', STATE=state, navitems=navitems)
 
-
-@app.route('/gconnect', methods=['GET', 'POST'])
+@app.route('/gconnect', methods=['POST'])
 def gconnect():
+    # Validate state token
     if request.args.get('state') != login_session['state']:
-        response = make_response(json.dumps('Invalid state parameter'), 401)
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
+    # Obtain authorization code
     code = request.data
+
     try:
+        # Upgrade the authorization code into a credentials object
         oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
         oauth_flow.redirect_uri = 'postmessage'
         credentials = oauth_flow.step2_exchange(code)
     except FlowExchangeError:
-        response = make_response(json.dumps('Failed to upgrade the authorization code'), 401)
-        response.header['Content-Type'] = 'application/json'
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
         return response
+
+    # Check that the access token is valid.
     access_token = credentials.access_token
-    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' %access_token)
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
     h = httplib2.Http()
     result = json.loads(h.request(url, 'GET')[1])
+    # If there was an error in the access token info, abort.
     if result.get('error') is not None:
         response = make_response(json.dumps(result.get('error')), 500)
         response.headers['Content-Type'] = 'application/json'
         return response
+
+    # Verify that the access token is used for the intended user.
     gplus_id = credentials.id_token['sub']
     if result['user_id'] != gplus_id:
-        response = make_response(json.dumps('Token does not match User ID'), 401)
-        response.header['Content-Type'] = 'application/json'
-        return response
-    if result['issued_to'] != CLIENT_ID:
-        response = make_response(json.dumps('Tokens Client ID does not match App'), 401)
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
+
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print "Token's client ID does not match app's."
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
     stored_access_token = login_session.get('access_token')
     stored_gplus_id = login_session.get('gplus_id')
     if stored_access_token is not None and gplus_id == stored_gplus_id:
-        response = make_response(json.dumps('Current user already connected'), 200)
+        response = make_response(json.dumps('Current user is already connected.'),
+                                 200)
         response.headers['Content-Type'] = 'application/json'
+        return response
 
+    # Store the access token in the session for later use.
     login_session['access_token'] = credentials.access_token
     login_session['gplus_id'] = gplus_id
 
+    # Get user info
     userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
     params = {'access_token': credentials.access_token, 'alt': 'json'}
     answer = requests.get(userinfo_url, params=params)
-    data = json.loads(answer.text)
 
-    login_session['username'] = data["name"]
-    login_session['picture'] = data["picture"]
-    login_session['email'] = data["email"]
+    data = answer.json()
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
 
     output = ''
     output += '<h1>Welcome, '
     output += login_session['username']
-    output += '</h1>'
-    flash("you are now logged in as %s" %login_session['username'])
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    flash("you are now logged in as %s" % login_session['username'])
+    print "done!"
     return output
-
 
 
 navitems = session.query(Category).order_by(asc(Category.name))
@@ -110,9 +135,9 @@ def showCategories():
 @app.route('/category/<string:url_name>/')
 @app.route('/category/<string:url_name>/products/')
 def showCategoryProducts(url_name):
-    category = session.query(Category).filter_by(url_name=url_name).one()
-    products = session.query(Product).filter_by(category_url=url_name).all()
-    return render_template('public-categoryproducts.html', navitems=navitems, category=category, products=products)
+	category = session.query(Category).filter_by(url_name=url_name).one()
+	products = session.query(Product).filter_by(category_url=url_name).all()
+	return render_template('public-categoryproducts.html', navitems=navitems, category=category, products=products)
 
 
 if __name__ == '__main__':
